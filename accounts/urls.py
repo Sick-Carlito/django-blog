732bddab4ed91a656740ff45230310fd13348a16
Sick-Carlito <carlb1319@gmail.com>
2025-09-12 00:12:10 +0300

    add password reset & change URLs using Django auth views

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ We use Django's built-in auth views for login/logout and a custom register view.
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import register_view
+from django.urls import reverse_lazy
 
 app_name = "accounts"
 
@@ -22,5 +23,60 @@ urlpatterns = [
         "logout/",
         auth_views.LogoutView.as_view(next_page="/"),
         name="logout",
+    ),
+
+# Password reset process:
+    # 1) password_reset - form to input email
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html",
+            email_template_name="registration/password_reset_email.html",
+            subject_template_name="registration/password_reset_subject.txt",
+            success_url=reverse_lazy("accounts:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    # 2) password_reset_done - displays message that email sent
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    # 3) password_reset_confirm - link in email directs here with uid/token
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html",
+            success_url=reverse_lazy("accounts:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    # 4) password_reset_complete - informs user the password is set
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+
+    # Password change (user must be logged in)
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/password_change_form.html",
+            success_url=reverse_lazy("accounts:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
     ),
 ]
